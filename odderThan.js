7f1fa210899d0c96d9269dc01b80f8eaca84507e
carlosdh1985 <50897615+carlosdh1985@users.noreply.github.com>
2019-10-14 00:38:40 -0400

    Create odderThan.js

---
+++ odderThan.js
@@ -0,0 +1,10 @@
+function oddOne(arr){
+    let result = -1;
+    for (let i = 0; i < arr.length; i++){
+            if(arr[i] % 2 !== 0){
+           result =  arr.indexOf(arr[i])
+                 }
+            
+}
+   return result    
+ } 
